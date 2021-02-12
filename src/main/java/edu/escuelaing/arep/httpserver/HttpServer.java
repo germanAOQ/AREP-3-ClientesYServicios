d7a45db10fb53473aa68d98fda2af6821ab942b2
Germán Ospina <german.ospina-q@mail.escuelaing.edu.co>
2021-02-11 23:18:34 -0500

    Configurando puerto

--- src/main/java/edu/escuelaing/arep/httpserver/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/httpserver/HttpServer.java
@@ -7,7 +7,7 @@ public class HttpServer {
 	public static void main(String[] args) throws IOException {
 		ServerSocket serverSocket = null;
 		try {
-			serverSocket = new ServerSocket(36000);
+			serverSocket = new ServerSocket(getPort());
 		} catch (IOException e) {
 			System.err.println("Could not listen on port: 35000.");
 			System.exit(1);
@@ -41,5 +41,12 @@ public class HttpServer {
 			clientSocket.close();
 		}
 		serverSocket.close();
+	}
+
+	private static int getPort() {
+		if (System.getenv("PORT") != null) {
+			return Integer.parseInt(System.getenv("PORT"));
+		}
+		return 36000;
 	}
 }
