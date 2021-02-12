7ccb523a23e45d75bb9d39af9481d67b739a5a4b
Germán Ospina <german.ospina-q@mail.escuelaing.edu.co>
2021-02-12 01:45:11 -0500

    Completado el retorno de páginas html

--- src/main/java/edu/escuelaing/arep/httpserver/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/httpserver/HttpServer.java
@@ -1,6 +1,7 @@
 package edu.escuelaing.arep.httpserver;
 
 import java.net.*;
+import java.util.Date;
 import java.io.*;
 
 public class HttpServer {
@@ -25,7 +26,32 @@ public class HttpServer {
 			}
 			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			String inputLine, outputLine;
+			String inputLine = in.readLine(), outputLine;
+			String[] requestParam = inputLine.split(" ");
+			String path = requestParam[1].replace("/", "");
+			if(path.equals("")) path = "src/main/resources/public/index.html";
+					
+			File file = new File(path);
+			BufferedReader bfr = null;
+			
+			if(!file.exists() || !file.isFile()) {
+				System.out.println("writing not found...");
+	             out.write("HTTP/1.0 200 OK\r\n");
+	             out.write(new Date() + "\r\n");
+	             out.write("Content-Type: text/html");
+			}else {
+				FileReader fr = new FileReader(file);
+				bfr = new BufferedReader(fr);
+				String output = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html\r\n" + "\r\n" + "<!DOCTYPE html>\n";
+				String line;	
+				while((line = bfr.readLine()) != null) {
+					output = output+line+"\n";
+				}
+				out.println(output);
+			}
+			if(bfr!=null) bfr.close();
+			
+			/**
 			while ((inputLine = in.readLine()) != null) {
 				System.out.println("Recibí: " + inputLine);
 				if (!in.ready()) {
@@ -35,7 +61,8 @@ public class HttpServer {
 			outputLine = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html\r\n" + "\r\n" + "<!DOCTYPE html>\n"
 					+ "<html>\n" + "<head>\n" + "<meta charset=\"UTF-8\">\n" + "<title>Title of the document</title>\n"
 					+ "</head>\n" + "<body>\n" + "<h1>Mi propio mensaje</h1>\n" + "</body>\n" + "</html>\n" + inputLine;
-			out.println(outputLine);
+			*/
+			//out.println(outputLine);
 			out.close();
 			in.close();
 			clientSocket.close();
