2a56cfe930053bb88d4acdc1d0746ea3be2db5c5
Germán Ospina <german.ospina-q@mail.escuelaing.edu.co>
2021-02-13 21:36:16 -0500

    Añadiendo persistencia

---
+++ src/main/java/edu/escuelaing/arep/persistence/Persistence.java
@@ -0,0 +1,51 @@
+package edu.escuelaing.arep.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Persistence {
+	private static final String url = "jdbc:postgresql://ec2-100-24-139-146.compute-1.amazonaws.com:5432/ddvgkl5cq2e61c";
+	private static final String user = "cxixtuwojbnwmn";
+	private static final String password = "dbf55124f4de8f399bf67f892832573717d130a18e8893269ab4f21c12776850";
+	
+	
+	public Connection connection() {
+		Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+	}
+	
+	public String getStatement(Connection conexion, String name) {
+		Statement statement;
+		ResultSet rs;
+		String res = null;
+		try {
+			statement = conexion.createStatement();
+			rs = statement.executeQuery("SELECT frase FROM func");
+			while(rs.next()) {
+				res = rs.getString("frase");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return res+" "+name;
+		
+	}
+	
+	public static void main(String[] args) {
+		Persistence per = new Persistence();
+		System.out.println(per.getStatement(per.connection(),"Germán"));
+	}
+
+}
